d6f85aa7f7387a55cae172d2af145dbd2f61886e
Thomas Sévère <thomas.severe@proton.me>
2024-06-06 12:53:41 +0200

    add multiple time tests

--- debugv2.R
+++ debugv2.R
@@ -11,19 +11,44 @@ if (!train) {
   date_pred <- as.character(dlgInput("Input date (YYYY-MM-dd): ")$res)
 }
 
- OccSL <- read.csv("data/SysGrid_500m_de_cote.csv") %>%
-      dplyr::select(c("X", "Y"))
-
-    OccSL$FID <- c(1:nrow(OccSL))
-    OccSL <- OccSL %>%
-      st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
-
-    OccSL_L93 <- OccSL %>%
-      st_transform(2154)
-    OccSL_L93$Nuit <- date_pred
-
 a  <- Sys.time()
-test <- list()
+test1 <- list()
 b <- Sys.time()
 
-print(b-a)
+
+OccSL <- read.csv("data/SysGrid_500m_de_cote.csv") %>%
+  dplyr::select(c("X", "Y"))
+
+c  <- Sys.time()
+test2 <- list()
+d <- Sys.time()
+
+OccSL$FID <- c(1:nrow(OccSL))
+OccSL <- OccSL %>%
+  st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
+
+e  <- Sys.time()
+test3 <- list()
+f <- Sys.time()
+
+
+OccSL_L93 <- OccSL %>%
+  st_transform(2154)
+OccSL_L93$Nuit <- date_pred
+
+g  <- Sys.time()
+test3 <- list()
+h <- Sys.time()
+
+print("Test 1 before loading csv")
+print(b - a)
+
+print("Test 2 after loading csv")
+print(d - c)
+
+print("Test 3 after sf")
+print(f - e)
+
+print("Test 2 after st_transform")
+print(h - g)
+
